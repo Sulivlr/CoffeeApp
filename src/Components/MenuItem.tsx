e391a75ef8917207e903be4a1865bd1342619b1a
Sultan Mukhtarov <suli419956@gmail.com>
2024-01-13 12:08:58 +0600

    Initial Commit. Created file 'Type.d.ts' for interfaces. Prepared propses for html.

---
+++ src/Components/MenuItem.tsx
@@ -0,0 +1,10 @@
+import {MenuProps} from "./type";
+
+
+const CoffeeMenu: React.FC<MenuProps> = ({name, price, image, count, onAdd, onDelete}) => {
+    return (
+        <div >
+
+        </div>
+    )
+}
