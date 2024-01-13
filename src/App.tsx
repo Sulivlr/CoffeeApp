05f0ced5432635377b31b4f7fccfedf14c8690ba
Sultan Mukhtarov <suli419956@gmail.com>
2024-01-13 14:22:23 +0600

    Initial Commit. Created file 'Type.d.ts' for interfaces. Prepared propses for html. Added Delete or remove file because forgot. Created a massive with the order objects. Changed the from Menu to Items and also started writing the logic of adding items to order menu. In the process, I decided to write the whole logic of adding and removing items in menu items cuz, i got confused in the process and yet dont know how to properly split them in different files. Created a logic of adding, deleting and summing total price of all items. I added whole code in app, cuz i cannot understand why the code is not working, and yet it didnt help.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,79 @@
 
 import './App.css'
+import {MenuProps, OrderMenu} from "./Components/type";
+import HamburgerImage from "./assets/Hamburger.png";
+import CheeseBurgerImage from "./assets/CheeseBurger.png";
+import FriesImage from "./assets/Fries.png";
+import CoffeeImage from "./assets/Coffee.png";
+import TeaImage from "./assets/Tea.png";
+import ColaImage from "./assets/Cola.png";
+import {useState} from "react";
 
 function App() {
 
+  const ITEMS: OrderMenu[] = [
+    {name: 'Hamburger', price: 80, count: 0, image: HamburgerImage},
+    {name: 'CheeseBurger', price: 90, count: 0, image: CheeseBurgerImage},
+    {name: 'Fries', price: 45, count: 0, image: FriesImage},
+    {name: 'Coffee', price: 70, count: 0, image: CoffeeImage},
+    {name: 'Tea', price: 50, count: 0, image: TeaImage},
+    {name: 'Cola', price: 40, count: 0, image: ColaImage},
+  ]
 
-  return (
-    <>
+  const MenuItems: React.FC = () => {
+    const [Items, setItems] = useState<OrderMenu[]>(ITEMS);
+    const AddItem = (index: number) => {
+      const addedItems = [...Items];
+      addedItems[index].count++;
+      setItems(addedItems);
+    };
 
-    </>
-  )
-}
+    const DeleteItem = (index: number) => {
+      const deletedItems = [...Items];
+      deletedItems[index].count = Math.max(0, deletedItems[index].count - 1);
+      setItems(deletedItems);
+    };
+
+    const TotalPrice = () => {
+      return Items.reduce((total, item) => {
+        return total + item.count * item.price;
+      }, 0);
+    }
+
+  }
+
+
+  const CoffeeItems: React.FC<MenuProps> = ({name, price, image, count, onAdd, onDelete}) => {
+    return (
+        <>
+          <div>
+            <img src={image} alt={name}/>
+            <p>{name}</p>
+            <p>Price: {price} KGS</p>
+            <p>Count: {count}</p>
+            <button onClick={onAdd}></button>
+            <button onClick={onDelete}></button>
+          </div>
+          <div className="coffee-container">
+            <div className="Items-list">
+              {ITEMS.map((item, index) => (
+                  <ITEMS
+                      key={index}
+                      name={item.name}
+                      price={item.price}
+                      image={item.image}
+                      count={item.count}
+                      onAdd={() => AddItem(index)}
+                      onDelete={() => deleteItem(index)}/>))}
+            </div>
+            <div className="order-detals">
+              <h2>Order Details</h2>
+              <p>Total Price: {TotalPrice()} KGS</p>
+            </div>
+          </div>
+        </>
+    );
+  };
+
 
 export default App
